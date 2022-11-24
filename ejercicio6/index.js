7b6757ebc5620652bc82287f695469b7d9626e9e
JP <jessicapaniagua1993@gmail.com>
2022-11-23 21:56:57 -0300

    Ejercicio6 - Actualizado

--- ejercicio6/index.js
+++ ejercicio6/index.js
@@ -2,21 +2,23 @@ let pedro={
     nombre: "Pedro Perez",
     edad: 30,
     activo: true,
-    hobbies: ["programar","squash","piano"],
-    estatura: "1.8",
-    saluda:()=>{
-        console.log("Hola, me llamo "+pedro.nombre)
-    }
+    hobbies: ["programar","squash","piano"]
 };
-
+//Imprimir Edad
 console.log(pedro.edad)
+//Add Estatura con valor 1.8
+pedro.estatura= "1.8"
+//Eliminar Activo
 delete pedro.activo
-console.log(pedro.activo)
-console.log("/////")
+//Imprimir
 for (let llave in pedro) {
     if (pedro.hasOwnProperty(llave)) {
       console.log(llave," : ",pedro[llave])
     }
 }
-
+//Add Saluda
+pedro.saluda=()=>{
+    console.log("Hola, me llamo "+pedro.nombre)
+}
+//Saludar
 console.log(pedro.saluda())
