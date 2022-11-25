5c587ec0687d32471b281ada85b570e67020bc7b
JP <jessicapaniagua1993@gmail.com>
2022-11-25 20:43:20 -0300

    Ejercicio7 - Actualizado

--- ejercicio7/index.js
+++ ejercicio7/index.js
@@ -14,12 +14,12 @@ receta.ingredientes.push({
     nombre: "Queso",
     cantidad: 1
 })
-
+//Imprimir Pan
 console.log(receta.ingredientes[0].nombre)
-
+//Sumar las cantidades
 let sum=0
 for (let i = 0; i < receta.ingredientes.length; i++) {
     sum += receta.ingredientes[i].cantidad;
 }
-
+//Imprimir suma
 console.log(sum)
